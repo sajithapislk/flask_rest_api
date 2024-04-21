645d6ea514b97fc4b4cdc5e0ddba320bbb752129
sajithapislk <sajith@apis.lk>
2024-04-21 14:44:36 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,111 @@
+import os
+import pymysql
+from http import HTTPStatus
+from flask_cors import CORS
+from flask import Flask, redirect, request, jsonify, url_for, abort
+from db import Database
+from config import DevelopmentConfig as devconf
+
+
+host = os.environ.get('FLASK_SERVER_HOST', devconf.HOST)
+port = os.environ.get('FLASK_SERVER_PORT', devconf.PORT)
+version = str(devconf.VERSION).lower()
+url_prefix = str(devconf.URL_PREFIX).lower()
+route_prefix = f"/{url_prefix}/{version}"
+
+
+def create_app():
+    app = Flask(__name__)
+    cors = CORS(app, resources={f"{route_prefix}/*": {"origins": "*"}})
+    app.config.from_object(devconf)
+    return app
+
+
+def get_response_msg(data, status_code):
+    message = {
+        'status': status_code,
+        'data': data if data else 'No records found'
+    }
+    response_msg = jsonify(message)
+    response_msg.status_code = status_code
+    return response_msg
+
+
+app = create_app()
+wsgi_app = app.wsgi_app
+db = Database(devconf)
+
+## ==============================================[ Routes - Start ]
+
+## /api/v1/getcity?country=IND
+@app.route(f"{route_prefix}/country", methods=['GET'])
+def getdata():
+    try:
+        countrycode = request.args.get('country', default='IND', type=str)
+        query = f"SELECT * FROM country WHERE id='{countrycode.upper()}'"
+        records = db.run_query(query=query)
+        response = get_response_msg(records, HTTPStatus.OK)
+        db.close_connection()
+        return response
+    except pymysql.MySQLError as sqle:
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(sqle))
+    except Exception as e:
+        abort(HTTPStatus.BAD_REQUEST, description=str(e))
+
+
+## /api/v1/getcitycodes
+@app.route(f"{route_prefix}/world", methods=['GET'])
+def getcitycodes():
+    try:
+        query = f"SELECT * FROM world"
+        records = db.run_query(query=query)
+        response = get_response_msg(records,  HTTPStatus.OK)
+        db.close_connection()
+        return response
+    except pymysql.MySQLError as sqle:
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(sqle))
+    except Exception as e:
+        abort(HTTPStatus.BAD_REQUEST, description=str(e))
+
+
+## /api/v1/health
+@app.route(f"{route_prefix}/health", methods=['GET'])
+def health():
+    try:
+        db_status = "Connected to DB" if db.db_connection_status else "Not connected to DB"
+        response = get_response_msg("I am fine! " + db_status, HTTPStatus.OK)       
+        return response
+    except pymysql.MySQLError as sqle:
+        abort(HTTPStatus.INTERNAL_SERVER_ERROR, description=str(sqle))
+    except Exception as e:
+        abort(HTTPStatus.BAD_REQUEST, description=str(e))
+
+## /
+@app.route('/', methods=['GET'])
+def home():
+    return redirect(url_for('health'))
+
+## =================================================[ Routes - End ]
+
+## ================================[ Error Handler Defined - Start ]
+## HTTP 404 error handler
+@app.errorhandler(HTTPStatus.NOT_FOUND)
+def page_not_found(e):    
+    return get_response_msg(data=str(e), status_code=HTTPStatus.NOT_FOUND)
+
+
+## HTTP 400 error handler
+@app.errorhandler(HTTPStatus.BAD_REQUEST)
+def bad_request(e):
+    return get_response_msg(str(e), HTTPStatus.BAD_REQUEST)
+
+
+## HTTP 500 error handler
+@app.errorhandler(HTTPStatus.INTERNAL_SERVER_ERROR)
+def internal_server_error(e):
+    return get_response_msg(str(e), HTTPStatus.INTERNAL_SERVER_ERROR)
+## ==================================[ Error Handler Defined - End ]
+
+if __name__ == '__main__':
+    ## Launch the application 
+    app.run(host=host, port=port)
---
+++ test_flask.sql
@@ -0,0 +1,97 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 21, 2024 at 11:11 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test_flask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `country`
+--
+
+CREATE TABLE `country` (
+  `id` int(8) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `country`
+--
+
+INSERT INTO `country` (`id`, `name`) VALUES
+(1, 'A'),
+(2, 'B');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `world`
+--
+
+CREATE TABLE `world` (
+  `id` int(8) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `world`
+--
+
+INSERT INTO `world` (`id`, `name`) VALUES
+(1, 'A'),
+(2, 'B');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `country`
+--
+ALTER TABLE `country`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `world`
+--
+ALTER TABLE `world`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `country`
+--
+ALTER TABLE `country`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `world`
+--
+ALTER TABLE `world`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
